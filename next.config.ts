9610e3a49a28766d0d9f4289682c56c27157846b
Vitalii Rybachuk <vitalii.rybachuk@infopulse.com>
2025-10-14 16:45:07 +0300

    update

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  transpilePackages: ["antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table"],
+};
+
+export default nextConfig;
